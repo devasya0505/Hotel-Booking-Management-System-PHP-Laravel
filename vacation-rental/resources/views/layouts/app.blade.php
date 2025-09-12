3d7ecb54416afbee3dd5d80ce812b22989a6ad36
devasya <devasyapatel555@gmail.com>
2025-09-12 22:03:17 +0530

    Added Model and Made changes in app.blade.php

--- vacation-rental/resources/views/layouts/app.blade.php
+++ vacation-rental/resources/views/layouts/app.blade.php
@@ -95,7 +95,7 @@
             <div class="container">
                 <div class="row justify-content-between">
                     <div class="col d-flex align-items-center">
-                        <p class="mb-0 phone"><span class="mailus">Phone no:</span> <a href="#">+00 1234 567</a> or <span class="mailus">email us:</span> <a href="#">emailsample@email.com</a></p>
+                        <p class="mb-0 phone"><span class="mailus">Phone no:</span> <a href="#">+91 65425 26485</a> or <span class="mailus">email us:</span> <a href="#">vacationrental534@gmail.com</a></p>
                     </div>
                     <div class="col d-flex justify-content-end">
                         <div class="social-media">
