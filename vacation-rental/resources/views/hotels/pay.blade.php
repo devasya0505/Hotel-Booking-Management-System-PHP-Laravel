b0091e30d0b5a75dc1055e367446079cc2c6c7f9
devasya <devasyapatel555@gmail.com>
2025-09-26 22:31:32 +0530

    added bg img to payment page

--- vacation-rental/resources/views/hotels/pay.blade.php
+++ vacation-rental/resources/views/hotels/pay.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="hero-wrap js-fullheight"
+        style="margin-top: -25px; background-image: url('{{ asset('assets/images/room-1.jpg') }}');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start"
+                data-scrollax-parent="true">
+                <div class="col-md-7 ftco-animate">
+                    <h2 class="subheading">Pay with PayPal</h2>
+                    <h1 class="mb-4"></h1>
+                    {{-- <p><a href="{{ route('home') }}" class="btn btn-primary">Go Home</a> </p> --}}
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <!-- Replace "test" with your own sandbox Business account app client ID -->
-        <script src="https://www.paypal.com/sdk/js?client-id=ARbKJ2qFgHQaIDl-ZntKVkuo_VFuFlGgy5jXg_kfopFUxdRJ_SWVGe4G6nd9U4CZNcKu_VuzX_WhC9_q&currency=USD"></script>
+        <script
+            src="https://www.paypal.com/sdk/js?client-id=ARbKJ2qFgHQaIDl-ZntKVkuo_VFuFlGgy5jXg_kfopFUxdRJ_SWVGe4G6nd9U4CZNcKu_VuzX_WhC9_q&currency=USD">
+        </script>
         <!-- Set up a container element for the button -->
         <div id="paypal-button-container"></div>
         <script>
