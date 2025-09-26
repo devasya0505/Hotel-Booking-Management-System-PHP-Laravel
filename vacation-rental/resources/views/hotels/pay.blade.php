8236632fb91ce75db4bd941101e7624cf860c4b3
devasya <devasyapatel555@gmail.com>
2025-09-27 04:26:13 +0530

    updated pay.blade.php

--- vacation-rental/resources/views/hotels/pay.blade.php
+++ vacation-rental/resources/views/hotels/pay.blade.php
@@ -2,26 +2,27 @@
 
 @section('content')
     <div class="hero-wrap js-fullheight"
-        style="margin-top: -25px; margin-bottom: 20px; background-image: url('{{ asset('assets/images/room-1.jpg') }}');">
+        style="margin-top: -25px; background-image: url('{{ asset('assets/images/room-1.jpg') }}');">
         <div class="overlay"></div>
         <div class="container">
             <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start"
                 data-scrollax-parent="true">
-                <div class="col-md-7 ftco-animate">
-                    <h1 class="mb-4">Pay with PayPal</h1>
+                <div class="col-md-12 ftco-animate">
+                    <h1 style="margin-left: 200px" class="subheading">Pay with PayPal</h1>
                     <h1 class="mb-4"></h1>
-                    {{-- <p><a href="{{ route('home') }}" class="btn btn-primary">Go Home</a> </p> --}}
+                    {{-- <p><a href="{{ route('home') }}" class="btn btn-primary">Go Home</a></p> --}}
                 </div>
             </div>
         </div>
     </div>
+    </div>
     <div class="container">
         <!-- Replace "test" with your own sandbox Business account app client ID -->
         <script
-            src="https://www.paypal.com/sdk/js?client-id=ARbKJ2qFgHQaIDl-ZntKVkuo_VFuFlGgy5jXg_kfopFUxdRJ_SWVGe4G6nd9U4CZNcKu_VuzX_WhC9_q&currency=USD">
+            src="https://www.paypal.com/sdk/js?client-id=AQ5SDUPoIRf92WldfbTu0cNWtIgJ66miCY3UNmNNdEOIWeMQQDNGe-0se_Gv7ikcfxll3sO27ke3xcxr&currency=USD">
         </script>
         <!-- Set up a container element for the button -->
-        <div id="paypal-button-container"></div>
+        <div style="margin-top: -220px; margin-left: 100px" id="paypal-button-container"></div>
         <script>
             paypal.Buttons({
                 // Sets up the transaction when a payment button is clicked
@@ -44,7 +45,5 @@
             }).render('#paypal-button-container');
         </script>
 
-    </div>
-    </div>
     </div>
 @endsection
