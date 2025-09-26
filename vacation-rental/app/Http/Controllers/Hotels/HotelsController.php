a18d2658969a30d1d7e36e2d6350939fa19b0d04
devasya <devasyapatel555@gmail.com>
2025-09-27 01:37:28 +0530

    updated hotelscontroller

--- vacation-rental/app/Http/Controllers/Hotels/HotelsController.php
+++ vacation-rental/app/Http/Controllers/Hotels/HotelsController.php
@@ -7,88 +7,79 @@ use Illuminate\Http\Request;
 use App\Models\Apartment\Apartment;
 use App\Models\Booking\Booking;
 use App\Models\Hotel\Hotel;
+use Auth;
 use DateTime;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Session;
+use Redirect;
+use Session;
 
 class HotelsController extends Controller
 {
     public function rooms($id)
     {
-        $getRooms = Apartment::select()->orderBy('id', 'desc')->take(6)->where('hotel_id', $id)->get();
+
+        $getRooms = Apartment::select()->orderBy('id', 'desc')->take(6)
+            ->where('hotel_id', $id)->get();
+
 
         return view('hotels.rooms', compact('getRooms'));
     }
 
-    public function roomsDetails($id)
+
+    public function roomDetails($id)
     {
+
         $getRoom = Apartment::find($id);
 
-        return view('hotels.roomsdetails', compact('getRoom'));
+
+        return view('hotels.room.details', compact('getRoom'));
     }
 
-
-
-    public function roomsBooking(Request $request, $id)
+    public function roomBooking(Request $request, $id)
     {
-
 
         $room = Apartment::find($id);
         $hotel = Hotel::find($id);
 
-        // Convert current date and request dates to d-m-Y format
-        $currentDate = date("d-m-Y");
-        $checkIn = date("d-m-Y", strtotime($request->check_in));
-        $checkOut = date("d-m-Y", strtotime($request->check_out));
 
-        if ($currentDate < $checkIn && $currentDate < $checkOut) {
-            if ($checkIn < $checkOut) {
 
-            $datetime1 = DateTime::createFromFormat('d-m-Y', $checkIn);
-            $datetime2 = DateTime::createFromFormat('d-m-Y', $checkOut);
-            $interval = $datetime1->diff($datetime2);
-            $days = $interval->format('%a'); // Number of days
+        if (strval(date("n/j/Y")) < strval($request->check_in) and strval(date("n/j/Y")) < strval($request->check_out)) {
 
-            $bookRooms = Booking::create([
-                "name" => $request->name,
-                "email" => $request->email,
-                "phone_number" => $request->phone_number,
-                "check_in" => $checkIn,
-                "check_out" => $checkOut,
-                "days" => $days,
-                "price" => $days * $room->price,
-                "user_id" => Auth::user()->id,
-                "room_name" => $room->name,
-                "hotel_name" => $hotel->name,
-            ]);
+            ///contine with logic
 
-            $totalPrice = $days * $room->price;
-            $price = Session::put('price', $totalPrice);
+            if ($request->check_in < $request->check_out) {
 
-            $getPrice = Session::get($price);
 
-            return Redirect::route('');
-            // echo "Booked Successfully";
-            // echo $currentDate . "<br>";
-            // echo $checkIn . "<br>";
-            // echo $checkOut . "<br>";
+                $datetime1 = new DateTime($request->check_in);
+                $datetime2 = new DateTime($request->check_out);
+                $interval = $datetime1->diff($datetime2);
+                $days = $interval->format('%a'); //now do whatever you like with $days
+
+                ///contine with logic
+                $bookRooms = Booking::create([
+
+                    "name" => $request->name,
+                    "email" => $request->email,
+                    "phone_number" => $request->phone_number,
+                    "check_in" => $request->check_in,
+                    "check_out" => $request->check_out,
+                    "days" => $days,
+                    "price" => $days * $room->price,
+                    "user_id" => Auth::user()->id,
+                    "room_name" => $room->name,
+                    "hotel_name" => $hotel->name,
+                ]);
+
+                $totalPrice = $days * $room->price;
+                $price = Session::put('price', $totalPrice);
+
+                $gePrice = Session::get($price);
+
+                return Redirect::route('hotel.pay');
             } else {
-            echo "Invalid Dates: Check-In date is GREATER THAN Check-Out date";
+                echo "check out date should be greater than check in date"
             }
         } else {
-            echo "DO NOT choose past dates" . "<br>";
-            // echo $currentDate . "<br>";
-            // echo $checkIn . "<br>";
-            // echo $checkOut . "<br>";
+            echo "choose dates in the future, invalid check in or check out dates"
         }
-    }
-
-    public function payWithPayPal(){
-        return view('hotels.pay');
-    }
-
-    public function success(){
-        return view('hotels.success');
     }
 }
